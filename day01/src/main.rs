245639792b195aa69297c1d55a6dbd6cde3faff0
Naïm Farji <>
2025-12-07 02:26:23 +0100

    Finished Part one of the problem added parsing to the string data that was extracted into tho vector First character checks wheter to subtract or add then check remaining characters to see how the dial value changes check to see if dial position is out of bounds and adjust the value accordingly print answer

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,24 +1,53 @@
-use std::fmt::Error;  
+use std::default;
+use std::fmt::Error;
 use std::fs::{File, read};
-use std::io::{self,BufRead};
+use std::io::{self, BufRead};
 
 fn main() -> std::io::Result<()> {
-    
-    let mut dialPosition:i32 = 50;
-    let mut DialInstructions:Vec<String> = Vec::new();
-    let file:File = File::open("DialInstructions.txt")?;
+    let mut dialPosition: i32 = 50;
+    let mut NeutralCounter = 0;
+    let mut DialInstructions: Vec<String> = Vec::new();
+    let file: File = File::open("DialInstructions.txt")?;
     let reader = io::BufReader::new(file);
 
     for line_result in reader.lines() {
-        let line:String = line_result?;
+        let line: String = line_result?;
         DialInstructions.push(line);
     }
 
     for instruction in DialInstructions {
-        println!("{instruction}");
+        match instruction.chars().next().unwrap() {
+            'R' => {
+                let rotationNumber: i32 = instruction[1..].parse().unwrap();
+                dialPosition += rotationNumber;
+                if (dialPosition > 99) {
+                    dialPosition %= 100;
+                }
+
+                if (dialPosition == 0) {
+                    NeutralCounter += 1;
+                }
+            }
+            'L' => {
+                let rotationNumber: i32 = instruction[1..].parse().unwrap();
+                dialPosition -= rotationNumber;
+                if (dialPosition < 0) {
+                    dialPosition %= 100;
+                    if dialPosition != 0 {
+                        dialPosition += 100;
+                    }
+                }
+
+                if (dialPosition == 0) {
+                    NeutralCounter += 1;
+                }
+            }
+            _ => panic!("Oh no!??!?!!?"),
+        }
+        dbg!(instruction, dialPosition);
     }
 
-    
+    println!("{NeutralCounter}");
 
     Ok(())
 }
