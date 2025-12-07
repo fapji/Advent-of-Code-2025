2ee88b084f314bb874e5f421ab88e9765def6225
Naïm Farji <>
2025-12-07 03:50:10 +0100

    Finished Part two of the problem Rewrote the match function: removed adding and position 0 added a check that adds the neutralCounter for every 100th position negative or positive if the dial was left at zero before subtracting we subtract 1 because it doesn't reach 0 when subtracting it leaves 0

--- day01/src/bin/part2.rs
+++ day01/src/bin/part2.rs
@@ -5,49 +5,48 @@ use std::io::{self, BufRead};
 
 fn main() -> std::io::Result<()> {
     let mut dialPosition: i32 = 50;
-    let mut NeutralCounter = 0;
-    let mut DialInstructions: Vec<String> = Vec::new();
+    let mut neutralCounter = 0;
+    let mut dialInstructions: Vec<String> = Vec::new();
     let file: File = File::open("DialInstructions.txt")?;
     let reader = io::BufReader::new(file);
 
     for line_result in reader.lines() {
         let line: String = line_result?;
-        DialInstructions.push(line);
+        dialInstructions.push(line);
     }
 
-    for instruction in DialInstructions {
+    for instruction in dialInstructions {
         match instruction.chars().next().unwrap() {
             'R' => {
                 let rotationNumber: i32 = instruction[1..].parse().unwrap();
                 dialPosition += rotationNumber;
                 if (dialPosition > 99) {
+                    neutralCounter += dialPosition / 100;
                     dialPosition %= 100;
-                }
-
-                if (dialPosition == 0) {
-                    NeutralCounter += 1;
                 }
             }
             'L' => {
                 let rotationNumber: i32 = instruction[1..].parse().unwrap();
+                if dialPosition == 0 {
+                    neutralCounter -= 1;
+                }
+
                 dialPosition -= rotationNumber;
-                if (dialPosition < 0) {
+
+                if (dialPosition <= 0) {
+                    neutralCounter += dialPosition / -100 + 1;
                     dialPosition %= 100;
                     if dialPosition != 0 {
                         dialPosition += 100;
                     }
                 }
-
-                if (dialPosition == 0) {
-                    NeutralCounter += 1;
-                }
             }
             _ => panic!("Oh no!??!?!!?"),
         }
-        dbg!(instruction, dialPosition);
+        dbg!(instruction, dialPosition, neutralCounter);
     }
 
-    println!("{NeutralCounter}");
+    println!("{neutralCounter}");
 
     Ok(())
 }
