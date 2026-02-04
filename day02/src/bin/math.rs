d37d517f3e45a644f5b2a723a10508543cfa92ca
Naïm Farji <>
2026-02-04 19:31:52 +0100

    test commit using ssh

--- day02/src/bin/math.rs
+++ day02/src/bin/math.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //test comment
     let originnum =11974834;
     let numdigits:f32 = f32::log(originnum as f32 , 10.0).ceil();
     let tophalfnum = originnum / 10i32.pow(numdigits as u32 / 2u32);
