9d509d6f89f257391bc61862efdc36a0fa091c05
Naïm Farji <>
2026-02-08 19:49:01 +0100

    Very close to working math function, Need to get the % statement correct.

--- day02/src/bin/math.rs
+++ day02/src/bin/math.rs
@@ -14,30 +14,42 @@ fn _split_at_half(num: i32) {
 }
 
 fn check_every_pattern(num: i32) -> bool {
-    let originnum = num;
     let mut is_invalid = false;
+    let originnum = num;
     let numdigits: f32 = f32::log(originnum as f32, 10.0).ceil();
     let maximum_cluster = (numdigits / 2.0).floor();
 
     for cluster_size in 1..maximum_cluster as i32 + 1 {
+        let mut is_invalid_per_clustersize = true;
         println!("Cluster Size: {}", { cluster_size });
         let amount_of_checks = numdigits as i32 / cluster_size;
         println!("amount_of_checks: {}", { amount_of_checks });
         let mut previous_cluster = 0;
-        for index in 1..amount_of_checks + 1 {
-            println!("Index: {}", { index });
+        for index in 0..amount_of_checks {
             //cluster_size is the size of the cluster index is the position you wanna take it out of if cluster_size is 2 index needs to move by 2
+            let current_cluster_halfway =
+                originnum / 10i32.pow(numdigits as u32 - (cluster_size * index+1) as u32);
+            //Now Add something with % (cluster_size needs to be cut on the right after the first index) 
             let current_cluster =
-                originnum / 10i32.pow(numdigits as u32 - (cluster_size * index) as u32); //Now Add something with % index - 1 times cluster_size
-            // let current_cluster =
-            //     current_cluster_halfway % 10i32.pow(((index - 1) * cluster_size) as u32);
+                current_cluster_halfway % 10i32.pow((index * cluster_size) as u32);
+
+
+            println!("Index: {}", index);
+            if index == 0 {
+                previous_cluster = current_cluster;
+            }
+
             println!("Current Cluster: {current_cluster}");
-            if previous_cluster == current_cluster && current_cluster != 0 {
-                is_invalid = true;
-                println!("Yipeee");
-                break;
+            if previous_cluster != current_cluster {
+                is_invalid_per_clustersize = false;
+                println!("Sadge");
+                //break;
             }
             previous_cluster = current_cluster;
+        }
+        if is_invalid_per_clustersize {
+            is_invalid = is_invalid_per_clustersize;
+         break;   
         }
     }
     //println!("amount of divisions: {}", { maximum_cluster });
