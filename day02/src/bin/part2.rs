d6bab86bd3bd1343575d2b362250a5caf016f3d4
Naïm Farji <>
2026-02-09 21:29:32 +0100

    Cleaned up some code

--- day02/src/bin/part2.rs
+++ day02/src/bin/part2.rs
@@ -38,17 +38,15 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn check_every_pattern(num: i64) -> bool {
+fn check_every_pattern(originnum: i64) -> bool {
     let mut is_invalid = false;
-    let originnum = num;
 
     //if the number is a power of 10 add 1
-    let numdigits: f32 = (num as f32).log10().floor() + 1.0;
-    // let numdigits: f32 = f32::log(originnum as f32, 10.0).floor() + 1;
-
+    let numdigits: f32 = (originnum as f32).log10().floor() + 1.0;
     let maximum_cluster = (numdigits / 2.0).floor();
 
     for cluster_size in 1..maximum_cluster as i32 + 1 {
+        //if the number of digits divided by the cluster size is a number without comma's do the checks (This checks if a repeating pattern is possible)
         if numdigits as i32 % cluster_size == 0 {
             let mut is_invalid_per_clustersize = true;
             // println!("Cluster Size: {}", { cluster_size });
@@ -56,18 +54,19 @@ fn check_every_pattern(num: i64) -> bool {
             // println!("amount_of_checks: {}", { amount_of_checks });
             let mut previous_cluster = 0;
             for index in 0..amount_of_checks {
-                //cluster_size is the size of the cluster index is the position you wanna take it out of if cluster_size is 2 index needs to move by 2
+                // println!("Index: {}", index);
+
+                //cluster_size * the size of the index is the position you wanna take it out of, if cluster_size is 2 index needs to move by 2
                 let mut current_cluster = originnum
                     / 10i64.pow(numdigits as u32 - (cluster_size * index + cluster_size) as u32);
                 //Now Add something with % (cluster_size needs to be cut on the right after the first index)
                 current_cluster = current_cluster % 10i64.pow((cluster_size) as u32);
 
-                // println!("Index: {}", index);
                 if index == 0 {
                     previous_cluster = current_cluster;
                 }
-
                 // println!("Current Cluster: {current_cluster}");
+
                 if previous_cluster != current_cluster {
                     is_invalid_per_clustersize = false;
                     // println!("Sadge");
@@ -81,12 +80,12 @@ fn check_every_pattern(num: i64) -> bool {
             }
         }
     }
-    //println!("amount of divisions: {}", { maximum_cluster });
+    // println!("amount of divisions: {}", { maximum_cluster });
 
     // if is_invalid {
-    //     println!("repeating pattern\n");
+    //     println!("repeating pattern \n");
     // } else {
-    //     println!("no\n");
+    //     println!("no \n");
     // }
 
     return is_invalid;
